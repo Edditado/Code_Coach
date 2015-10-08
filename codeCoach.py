bef5c78242ab02cc30f74e9a94aef855c7714b6e
cindyeleana <eli92_sp@hotmail.com>
2015-10-08 12:01:53 -0500

    Guardar archivo

--- codeCoach.py
+++ codeCoach.py
@@ -2,7 +2,7 @@
 #encoding: utf8
 
 import gtk
-
+import os
 
 class FileChooserDialog:
     filename=""
@@ -24,6 +24,25 @@ class FileChooserDialog:
             self.filename = filechooserdialog.get_filename()
             
         filechooserdialog.destroy()
+
+
+        
+class confirmOverwriteFileDialog:
+    cansave = False
+    def __init__(self,filename, parent):
+        dialog = gtk.Dialog(( 'Confirmar' ), parent, gtk.DIALOG_MODAL | gtk.DIALOG_DESTROY_WITH_PARENT, ( gtk.STOCK_YES, gtk.RESPONSE_ACCEPT, gtk.STOCK_NO, gtk.RESPONSE_REJECT ) )
+        dialog.vbox.pack_start( gtk.Label(( 'El archivo "%s" ya existe' ) % ( filename, ) ) )
+        dialog.vbox.pack_start( gtk.Label(( 'Desea reemplazarlo?' ) ) )
+        
+        dialog.show_all()
+        response = dialog.run()
+        if response == gtk.RESPONSE_ACCEPT:
+            self.cansave = True
+            dialog.destroy()
+        else:
+            dialog.destroy()
+        
+
 
 
 class MainWindow: 
@@ -49,6 +68,7 @@ class MainWindow:
         
         btnNew.connect("clicked", self.newDoc)
         btnOpen.connect("clicked", self.openwindow)
+        btnSave.connect("clicked", self.savewindow)
         toolbar.insert(btnNew, 0)
         toolbar.insert(separator1, 1)
         toolbar.insert(btnOpen, 2)
@@ -58,8 +78,11 @@ class MainWindow:
         extContainer.pack_start(toolbar, expand=False)
 
         self.notebook = gtk.Notebook()
+        sw = gtk.ScrolledWindow()
+        sw.set_policy(gtk.POLICY_AUTOMATIC, gtk.POLICY_AUTOMATIC)
         textArea = gtk.TextView()
-        self.notebook.append_page(textArea, gtk.Label("Empty document"))
+        sw.add(textArea)
+        self.notebook.append_page(sw, gtk.Label("Empty document"))
 
         extContainer.pack_start(self.notebook)
         
@@ -77,7 +100,10 @@ class MainWindow:
 
 
     def newDoc(self, widget):
+        sw = gtk.ScrolledWindow()
+        sw.set_policy(gtk.POLICY_AUTOMATIC, gtk.POLICY_AUTOMATIC)
         newTextArea = gtk.TextView()
+        sw.add(newTextArea)
         nPages = self.notebook.get_n_pages()
         numDoc = 0
         for i in xrange(nPages):
@@ -86,10 +112,10 @@ class MainWindow:
             if labelName[0] == "Empty document":
                 numDoc += 1
         if numDoc == 0:
-            self.notebook.append_page(newTextArea, gtk.Label("Empty document"))
+            self.notebook.append_page(sw, gtk.Label("Empty document"))
 
         else: 
-            self.notebook.append_page(newTextArea, gtk.Label("Empty document("+str(numDoc)+")"))
+            self.notebook.append_page(sw, gtk.Label("Empty document("+str(numDoc)+")"))
 
         self.window.show_all()
         self.notebook.set_page(-1)
@@ -111,7 +137,77 @@ class MainWindow:
         self.notebook.append_page(sw, gtk.Label(flname))
         self.window.show_all()
         self.notebook.set_page(-1)
+
+
+    def savewindow(self,widget):
         
+        text_filter=gtk.FileFilter()
+        text_filter.set_name("Archivos de texto")
+        text_filter.add_mime_type("text/*")
+        all_filter=gtk.FileFilter()
+        all_filter.set_name("Todos")
+        all_filter.add_pattern("*")
+        filename=None
+        dialog=gtk.FileChooserDialog(title="Guardar archivo", action=gtk.FILE_CHOOSER_ACTION_SAVE,
+            buttons=(gtk.STOCK_CANCEL, gtk.RESPONSE_CANCEL, gtk.STOCK_SAVE, gtk.RESPONSE_OK))
+          
+        if (text_filter != None) and (all_filter != None):
+            dialog.add_filter(text_filter)
+            dialog.add_filter(all_filter)
+            
+        response = dialog.run()
+        if response == gtk.RESPONSE_CANCEL:
+            dialog.destroy()
+                
+        if response == gtk.RESPONSE_OK:
+
+            cansave= False
+
+            filename = dialog.get_filename()
+            pageNum = self.notebook.get_current_page()
+            textviewCurrent = self.notebook.get_nth_page(pageNum)
+            textviewCurrent=textviewCurrent.get_child()
+            textbuffer=textviewCurrent.get_buffer()
+            start_iter = textbuffer.get_start_iter()
+            end_iter = textbuffer.get_end_iter()
+            newcode= textbuffer.get_text(start_iter, end_iter, True)
+            
+
+            if os.path.exists(dialog.get_filename()) == True:
+               
+                dialog2=confirmOverwriteFileDialog(filename, dialog)
+
+                if dialog2.cansave == True:
+                    cansave = True
+                    if filename != None:
+                        fileNew=open(filename, 'w')
+                        fileNew.write(newcode)
+                        fileNew.close()
+                else:
+                    dialog.destroy()
+                    
+            else:
+                cansave = True
+            if cansave == True: 
+                if filename != None:
+                    fileNew=open(filename, 'w')
+                    fileNew.write(newcode)
+                    fileNew.close()
+                dialog.destroy()
+            else:
+                pass
+
+    
+
+
+           
+        
+  
+
+
+
+    
+            
 
 MainWindow()
 gtk.main()
